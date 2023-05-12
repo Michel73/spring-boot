2f692792e99d0f5092f742648ca747ae96b6685a
Michael Kopf <michael.kopf@sealsystems.de>
2023-05-12 17:53:41 +0200

    Initial commit.

---
+++ src/main/java/com/example/springboot/Application.java
@@ -0,0 +1,23 @@
+package com.example.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
+
+@RestController("")
+class MyController {
+  @GetMapping
+  public String modifyMe() {
+    return "mod-2";
+  }
+}
